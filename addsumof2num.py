319e8001feeb0c7beb029a5de51c677b887e1f34
Balaji E <124427573+CodeByBalajiE@users.noreply.github.com>
2025-01-28 20:56:14 +0530

    day 1 coding task

--- addsumof2num.py
+++ addsumof2num.py
@@ -1,3 +1,10 @@
+"""2.You have two numbers, and your challenge is to write a program that performs both addition and subtraction with them. 
+However, if any subtraction results in a negative number, display it as a positive value. How will you tackle this and 
+show the final results?For example, consider two numbers 20 and 15. Addition of 2 values: 20 + 15 = 35.Subtraction of 
+2 values: 20 - 15 = 5. For example, consider two numbers 20 and -150. Addition of 2 values: 20 + (-150) = -130 Absolute
+value of (-130) = 130.Subtraction of 2 values: 20 - (-150) = 170."""
+
+
 #day1 coding task program 1
 #add and sub of two num 
 a=int(input("enter the first num")) #first num
