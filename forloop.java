6bdda7e762508579b467eaa4c54a73d31739aeae
Balaji E <124427573+CodeByBalajiE@users.noreply.github.com>
2025-01-30 18:17:13 +0530

    day 3 coding task

---
+++ forloop.java
@@ -0,0 +1,10 @@
+/*Imagine you need to repeat a cheerful message. Write a program that uses a for 
+loop to print "ALL IS WELL" exactly twenty times. How will you set up your loop to 
+ensure this message appears the right number of times?*/
+public class forloop {
+    public static void main(String[] args) {
+        for(int i=1;i<=20;i++){
+            System.out.println(i+".ALL IS WELL");
+        }
+    }
+}
